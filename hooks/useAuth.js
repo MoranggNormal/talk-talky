d3d9a72fa11d849a5dc0099bfa4f2cf73dd6c8d4
Euller Peixoto <peixotoeuller500@gmail.com>
2022-01-17 20:26:46 -0200

    setup firebase

---
+++ hooks/useAuth.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect, useContext, createContext } from "react";
+
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+
+const authContext = createContext();
+
+// Provider component that wraps your app and makes auth object ...
+// ... available to any child component that calls useAuth().
+export function ProvideAuth({ children }) {
+  const auth = useProvideAuth();
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+}
+
+// Hook for child components to get the auth object ...
+// ... and re-render when it changes.
+export const useAuth = () => {
+  return useContext(authContext);
+};
+
+function useProvideAuth() {
+  const auth = getAuth();
+
+  const [user, setUser] = useState(null);
+
+  // Wrap any Firebase methods we want to use making sure ...
+  // ... to save the user to state.
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+
+    return signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
+  const signOutt = () => {
+    return signOut(auth)
+      .then(() => {})
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // Subscribe to user on mount
+  // Because this sets state in the callback it will cause any ...
+  // ... component that utilizes this hook to re-render with the ...
+  // ... latest auth object.
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(false);
+      }
+    });
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+  // Return the user object and auth methods
+  return {
+    user,
+    signInWithGoogle,
+    signOutt,
+  };
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,26 @@
+import { app, database } from "../services/firebase";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+import { useAuth } from "../hooks/useAuth";
+
+import { useState, useEffect } from "react";
+
 export default function Home() {
-  return <div>haha</div>;
+  const [test, setTest] = useState([]);
+
+  const { user, signInWithGoogle, signOutt } = useAuth();
+
+  console.log(user);
+
+  return (
+    <>
+      {user ? (
+        <button onClick={signOutt}>SignOut</button>
+      ) : (
+        <button onClick={signInWithGoogle}>Login</button>
+      )}
+
+      {user && <h1>Olá, {user.displayName}!</h1>}
+    </>
+  );
 }
